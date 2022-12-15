73a8d73e0564065fac9af056df326f677df39c86
ORTEM <43004701+Abbas-OR@users.noreply.github.com>
2022-12-15 23:53:01 +0330

    Update page.py

--- page.py
+++ page.py
@@ -9,7 +9,7 @@ def home():
     return render_template("index.html") 
 if __name__ == '__main__':
     #< just edit here
-    host = "127.0.0.1" # type your domain or server ip
-    port = 80          # type a port (http://your-domain.com:port) or (http://your-server-id:port)
+    host = "127.0.0.1" #line 12: type your domain or server ip 
+    port = 80          #line 13: type a port (http://your-domain.com:port) or (http://your-server-id:port)
     #just edit here />
     app.run(debug=True, port=port, host=host)
